07a2fe89ef26744e8ebd771e5fafb7fc430654bf
xwta <110807199+xwta@users.noreply.github.com>
2026-06-19 23:47:18 +0800

    miniprogram: use environment api config

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,11 +1,14 @@
+const { getEnvConfig } = require('./config/env');
+
 App({
   globalData: {
-    apiBaseUrl: 'https://example.com/api',
+    apiBaseUrl: getEnvConfig().apiBaseUrl,
     userInfo: null,
     campus: null
   },
 
   onLaunch() {
     console.log('校园零食小程序启动');
+    console.log('当前接口地址:', this.globalData.apiBaseUrl);
   }
 });
