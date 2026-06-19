b5f1786604c847e7c453e32a0fd895b696cdabb1
xwta <110807199+xwta@users.noreply.github.com>
2026-06-20 00:08:15 +0800

    admin: add mutation api methods

--- admin/src/api.js
+++ admin/src/api.js
@@ -17,12 +17,32 @@ export function createProduct(data) {
   return http.post('/admin/products', data).then(res => res.data);
 }
 
+export function updateProduct(id, data) {
+  return http.put(`/admin/products/${id}`, data).then(res => res.data);
+}
+
+export function deleteProduct(id) {
+  return http.delete(`/admin/products/${id}`).then(res => res.data);
+}
+
 export function fetchOrders() {
   return http.get('/admin/orders').then(res => res.data);
 }
 
+export function updateOrderStatus(id, data) {
+  return http.put(`/admin/orders/${id}/status`, data).then(res => res.data);
+}
+
 export function fetchAgents() {
   return http.get('/admin/agents').then(res => res.data);
+}
+
+export function createAgent(data) {
+  return http.post('/admin/agents', data).then(res => res.data);
+}
+
+export function updateAgentStatus(id, data) {
+  return http.put(`/admin/agents/${id}/status`, data).then(res => res.data);
 }
 
 export function fetchAgentSummary() {
